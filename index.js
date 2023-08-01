4427210e5655703db7240b863ee6823b903bba62
diaken2 <107970623+diaken2@users.noreply.github.com>
2023-08-01 07:06:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require("./routes/auth.route")
 app.use(function(req, res, next) {
 
 
-  res.header("Access-Control-Allow-Origin","*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin","http://a0847058.xsph.ru"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
   
